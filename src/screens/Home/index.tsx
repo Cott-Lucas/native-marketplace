348b3dbc97ef13acb2371e2f080cdefde0cfdc46
cott-lucas <lucasottaviani97@outlook.com>
2023-11-24 13:56:11 -0300

    start

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Header } from 'react-native/Libraries/NewAppScreen'
+import { Container } from './styled'
+
+const Home = () => {
+  return (
+    <Container>
+      <Header/>
+    </Container>
+  )
+}
+
+export default Home
