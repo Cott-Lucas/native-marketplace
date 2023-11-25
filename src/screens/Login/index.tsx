ac15cb576f8554d93e291d00950de2b2cdb9eeea
cott-lucas <lucasottaviani97@outlook.com>
2023-11-25 13:06:32 -0300

    finalização pagina de login

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,17 +1,59 @@
+import React from "react";
+import BackIcon from "../../components/common/BackIcon";
+import {
+  Bold,
+  CompanyLogo,
+  Container,
+  ForgetPassword,
+  Input,
+  InputContainer,
+  Logo,
+  RegisterText,
+} from "./styled";
+import DefaultButton from "../../components/common/DefaultButton";
+import { Alert } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { PropsStack } from "../../routes";
 
-import React from 'react'
-import BackIcon from '../../components/common/BackIcon'
-import { Container, Logo } from './styled'
-
-const logo = require('../../../assets/images/logo.png')
+const logo = require("../../../assets/images/logo.png");
+const compLogo = require("../../../assets/images/logo-obc.png");
 
 const Login = () => {
+  const navigation = useNavigation<PropsStack>()
+
+  const handleLogin = () => {
+    Alert.alert("botão de login");
+  };
+
   return (
     <Container>
-      <BackIcon marginLeft={30}/>
-      <Logo source={logo}/>
+      <BackIcon marginLeft={30} />
+      <Logo source={logo} />
+      <InputContainer>
+        <Input placeholder="Digite seu email" placeholderTextColor="white" />
+      </InputContainer>
+      <InputContainer>
+        <Input
+          placeholder="Digite sua Senha"
+          placeholderTextColor="white"
+          secureTextEntry={true}
+        />
+      </InputContainer>
+      <ForgetPassword>Esqueceu sua senha?</ForgetPassword>
+      <DefaultButton
+        buttonText="Fazer Login"
+        buttonType="primary"
+        marginVertical={40}
+        buttonHandle={() => handleLogin()}
+      />
+      <RegisterText onPress={()=> {
+        navigation.navigate("Home")
+      }}>
+        Você ainda não tem conta? <Bold>Registre-se aqui!</Bold>
+      </RegisterText>
+      <CompanyLogo source={compLogo}/>
     </Container>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
--- src/screens/Login/styled.ts
+++ src/screens/Login/styled.ts
@@ -1,18 +1,62 @@
 import styled from "styled-components/native";
-import Constants from 'expo-constants';
+import Constants from "expo-constants";
 
 const statusBarHeight = Constants.statusBarHeight;
 
-
-export const Container = styled.View`
-  flex:1 ;
-  background-color: ${({theme}) => theme.colors.backgroundLight};
-  padding-top: ${statusBarHeight+ 20}px;
-`
+export const Container = styled.ScrollView`
+  flex: 1;
+  background-color: ${({ theme }) => theme.colors.backgroundLight};
+  padding-top: ${statusBarHeight + 20}px;
+`;
 export const Logo = styled.Image.attrs({
-  resizeMode: 'contain',
+  resizeMode: "contain",
 })`
   width: 230px;
-  height: 230px;
+  height: 190px;
   margin: 0 auto;
-`
+`;
+
+export const InputContainer = styled.View`
+  width: 85%;
+  height: 40px;
+  border: 1px solid ${({ theme }) => theme.colors.borderColor};
+  background-color: ${({ theme }) => theme.colors.background};
+  border-radius: 5px;
+  margin: 0 auto;
+  margin-top: 20px;
+`;
+
+export const Input = styled.TextInput`
+  flex: 1;
+  font-size: 18px;
+  color: ${({ theme }) => theme.colors.primaryText};
+  padding-left: 10px;
+`;
+
+export const ForgetPassword = styled.Text`
+  font-size: 16px;
+  font-weight: bold;
+  text-align: right;
+  color: ${({ theme }) => theme.colors.secondaryText};
+  margin-right: 10%;
+`;
+
+export const RegisterText = styled.Text`
+  font-size: 14px;
+  text-align: center;
+  color: ${({ theme }) => theme.colors.secondaryText};
+`;
+
+export const Bold = styled.Text`
+  font-size: 14px;
+  font-weight: bold;
+  color: ${({ theme }) => theme.colors.primaryText};
+`;
+
+export const CompanyLogo = styled.Image.attrs({ 
+  resizeMode: "contain" 
+})`
+  width:100px;
+  margin: 0 auto;
+  margin-top:50px;
+`;
