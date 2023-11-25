a94491990fa78dd8defe21502dfdf29461f4f65c
cott-lucas <lucasottaviani97@outlook.com>
2023-11-25 11:41:09 -0300

    Lista de produto

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Header from "../../components/common/Header";
 import { Container } from './styled'
 import NavBar from '../../components/common/NavBar';
+import ProductList from '../../components/common/ProductList';
 
 const Home = () => {
   return (
     <Container>
       <Header/>
+      <ProductList/>
       <NavBar/>
     </Container>
   )
